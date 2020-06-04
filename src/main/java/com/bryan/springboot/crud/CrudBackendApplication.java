2b1963570ae97a94abc678c27a75b7588cbf6bbb
BDDLR <bryan.dominguez.delarosa@gmail.com>
2020-06-03 22:09:06 -0500

    Testing commit from IDE, adding Person entity

--- src/main/java/com/bryan/springboot/crud/CrudBackendApplication.java
+++ src/main/java/com/bryan/springboot/crud/CrudBackendApplication.java
@@ -1,4 +1,4 @@
-package com.bryan.springboot.web;
+package com.bryan.springboot.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/bryan/springboot/crud/model/Person.java
@@ -0,0 +1,70 @@
+package com.bryan.springboot.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column
+	private String address;
+	
+	@Column
+	private String telephone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+
+}
